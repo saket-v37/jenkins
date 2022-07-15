97feb446dc64f29a5df3283380a25461f429f6ca
Saket Verma <saketverma852@gmail.com>
2022-07-15 16:14:39 +0530

    first commit

---
+++ src/main/java/com/example/jenkins_exercise/JenkinsExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsExerciseApplication.class, args);
+	}
+
+}
